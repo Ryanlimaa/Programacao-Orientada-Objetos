7da24f74e34194ba4144f6825e4c2606a8753d54
Ryanlimaa <ryan.limaa@outlook.com>
2025-02-11 14:38:50 -0300

    Entrada e saida

---
+++ Comandos de Entrada e Saida/ex02.py
@@ -0,0 +1,9 @@
+codigo = 105
+nome = "Ryan Lima"
+salario = 1650.00
+ativo = True
+
+print("codigo: %d " % codigo)
+print("Nome: %s " % nome)
+print("Salario: %.2f " % salario)
+print("Ativo: %r " % ativo)
