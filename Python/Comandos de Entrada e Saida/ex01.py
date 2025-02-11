b1a69616211e710208fad558da223421eac19daa
Ryanlimaa <ryan.limaa@outlook.com>
2025-02-11 14:53:53 -0300

    Entrada e saida

---
+++ Python/Comandos de Entrada e Saida/ex01.py
@@ -0,0 +1,11 @@
+codigo = 10  
+salario = 1500.00
+nome = "Jose"
+situacao = True
+
+tipo = type (salario)
+
+print(salario)
+print(tipo)
+
+print("Codigo:", codigo, "Nome:", nome, ", O salario atual é de:", salario)
---
+++ Python/Comandos de Entrada e Saida/ex03.py
@@ -0,0 +1,9 @@
+codigo = int(input("Digite o codigo de acesso: "))
+nome = input("Digite o nome de usuario: ")
+salario = float(input("Informe o salario: "))
+ativo = True
+
+print("Codigo: %d" % codigo)
+print("Nome: %s" % nome)
+print("Salario %.2f" % salario)
+print("Ativo %r" % ativo)
