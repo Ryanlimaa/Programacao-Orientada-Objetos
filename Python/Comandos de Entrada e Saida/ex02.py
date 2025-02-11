2b3d3b14bfead416bbf42bec5c4531df49181824
Ryanlimaa <143458483+Ryanlimaa@users.noreply.github.com>
2025-02-11 14:26:19 -0300

    Delete Python/Comandos de Entrada e Saida directory

--- Python/Comandos de Entrada e Saida/ex02.py
+++
@@ -1,9 +0,0 @@
-codigo = 105
-nome = "Ryan Lima"
-salario = 1650.00
-ativo = True
-
-print("codigo: %d " % codigo)
-print("Nome: %s " % nome)
-print("Salario: %.2f " % salario)
-print("Ativo: %r " % ativo)
--- Python/Comandos de Entrada e Saida/ex03.py
+++
@@ -1,9 +0,0 @@
-codigo = int(input("Digite o codigo de acesso: "))
-nome = input("Digite o nome de usuario: ")
-salario = float(input("Informe o salario: "))
-ativo = True
-
-print("Codigo: %d" % codigo)
-print("Nome: %s" % nome)
-print("Salario %.2f" % salario)
-print("Ativo %r" % ativo)
